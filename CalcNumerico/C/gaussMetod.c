086890f311ebc10904f941042d7a9a57501e9215
DaviPernalonga30 <davi.coelho_comp@outlook.com>
2022-10-26 18:54:18 +0000

    criação do gaussMetod.h

--- CalcNumerico/C/gaussMetod.c
+++ CalcNumerico/C/gaussMetod.c
@@ -1,4 +1,22 @@
-#include<stdio.h>
+#include</gaussMetod.h>
+
+float setMatrix(int i, int j){
+	float Matrix[i][j];
+	for(int a = 0; a<i; a++){
+		for(int b = 0; b<j; b++){
+		printf("posicao [%d][%d]", &a, &b);
+		scanf("%f", Matrix[a][b]);
+		}
+
+
+	}
+	return Matrix;
+}
+
+
+
 int main(){
+	Ma[3][3] = setMatrix(3, 3);
 	return 0;
 }
+
---
+++ CalcNumerico/C/gaussMetod.h
@@ -0,0 +1,9 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+float gaussElim(float matrix[int i][int j]);
+void printArr(float matrix[int i][int j]);
+void printResult();
+float setMatrix();
+
+
